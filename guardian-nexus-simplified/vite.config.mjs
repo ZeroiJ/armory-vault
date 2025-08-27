a2e71fd035f09b6c8c2516ae6f3116d4370e5f00
ZeroiJ <your.email@example.com>
2025-08-28 00:53:28 +0530

    fix: specify input for rollupOptions in vite.config.mjs

---
+++ guardian-nexus-simplified/vite.config.mjs
@@ -0,0 +1,30 @@
+import { defineConfig, loadEnv } from "vite";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import tagger from "@dhiwise/component-tagger";
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return defineConfig({
+    base: env.VITE_BASE || '/guardian_nexus/',
+    // This changes the out put dir from dist to build
+    // comment this out if that isn't relevant for your project
+    build: {
+      outDir: "dist",
+      chunkSizeWarningLimit: 2000,
+      rollupOptions: {
+        input: {
+          main: 'index.html'
+        }
+      }
+    },
+    plugins: [tsconfigPaths(), react(), tagger()],
+    server: {
+      port: "4028",
+      host: "0.0.0.0",
+      strictPort: true,
+      allowedHosts: ['.amazonaws.com', '.builtwithrocket.new']
+    }
+  });
+};
