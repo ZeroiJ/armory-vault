8416f4bffd4e2c4db3587cda58ce61bec09b1ca7
ZeroiJ <your.email@example.com>
2025-08-28 16:39:58 +0530

    feat: add back authentication page

--- guardian_nexus/guardian_nexus/src/Routes.jsx
+++ guardian_nexus/guardian_nexus/src/Routes.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import AuthenticationAuthorization from "./pages/authentication-authorization";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
+        <Route path="/" element={<AuthenticationAuthorization />} />
+        <Route path="/auth/callback" element={<AuthenticationAuthorization />} />
       </Routes>
     </BrowserRouter>
   );
